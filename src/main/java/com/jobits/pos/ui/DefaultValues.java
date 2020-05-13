7116417fc9f0c0a841cfdffd20bee274052030e0
Jorge Hurtado <jjhurtado796@gmail.com>
2020-05-13 17:37:29 -0400

    Valores agregados

--- src/main/java/com/jobits/pos/ui/DefaultValues.java
+++ src/main/java/com/jobits/pos/ui/DefaultValues.java
@@ -3,21 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.jobits.pos.ui;
 
 import java.awt.Color;
 import util.materials.MaterialColors;
 
 /**
- * 
+ *
  * JoBits
+ *
  * @author Jorge
- * 
+ *
  */
 public class DefaultValues {
 
     public static final Color BUTTON_COLOR = MaterialColors.INDIGO_500;
     public static int BORDER_LINE_THICK = 3;
-    
+    public static Color SECONDARY_PANEL_COLOR = MaterialColors.INDIGO_100;
+
 }
