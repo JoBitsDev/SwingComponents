86455675c58c01eb21485aa2d7d7dc5644f73997
Erik <erikquesada23@gmail.com>
2021-05-08 00:35:41 -0400

    enum para tipos de temas

---
+++ src/main/java/com/jobits/ui/themes/ThemeType.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jobits.ui.themes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Home
+ */
+public enum ThemeType {
+
+    DEFAULT("Default"),
+    SIMPLE_MATERIAL("Material Simple"),
+    //    DARK_MATERIAL("Material Oscuro"), TODO: en progreso tema oscuro
+    MATERIAL("Material");
+
+    private final String themeName;
+
+    private ThemeType(String themeName) {
+        this.themeName = themeName;
+    }
+
+    public String getThemeName() {
+        return themeName;
+    }
+
+    public static List<String> stringValues() {
+        List<String> aux = new ArrayList<>();
+        for (ThemeType v : ThemeType.values()) {
+            aux.add(v.getThemeName());
+        }
+        return aux;
+    }
+
+}
